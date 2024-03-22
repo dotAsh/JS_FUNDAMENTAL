9afed20b8eca8bea2bc7e55c1e78d1d0bba758df
dotAsh <ashikshuvo45@gmail.com>
2024-03-23 02:23:41 +0600

    -1 added in file4 after length

--- Assignment_04.js
+++ Assignment_04.js
@@ -3,8 +3,8 @@ function checkEnding(str1, str2) {
     if(str1.length  < str2.length){
         return false;
     }
-    let i = str1.length;
-    for(let j = str2.length;j >= 0;j--){
+    let i = str1.length - 1;
+    for(let j = str2.length - 1;j >= 0;j--){
         if(str1[i] !== str2[j]){
             return false;
         }
